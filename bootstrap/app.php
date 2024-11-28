e6b9cb12eaf60728575edc7e97c7f2624fe86eab
JohnJunqueira <johncba2015@hotmail.com>
2024-11-28 00:57:37 -0300

    Criando Middlewares, Controllers, Autenticações, Alias de Midlleware, dashboards de users

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Middleware\Admin;
+use App\Http\Middleware\Prestador;
+use App\Http\Middleware\Usercliente;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Auth;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\Admin::class,
+            'prestador' => \App\Http\Middleware\Prestador::class,
+            'usercliente' => \App\Http\Middleware\Usercliente::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
+use App\Http\Controllers\Backend\PrestadorController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +20,12 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//Rota admin
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
+->middleware(['auth', 'admin'])
+->name('admin.dashboard');
+
+//Rota prestador
+Route::get('prestador/dashboard', [PrestadorController::class, 'dashboard'])->middleware(['auth', 'prestador'])
+->name('prestador.dashboard');
